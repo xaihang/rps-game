aed92981a4778b894de98cb02e88babe2cdec168
xaihang <xaihang12@gmail.com>
2022-10-10 15:12:47 -0500

    else if statements to js

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
-var win = 0;
-var lose = 0;
-var draw = 0;
+var wins = 0;
+var loses = 0;
+var draws = 0;
 
 // options for computer to select 
 var options = ["Rock", "Paper", "Scissor"]
 
-//options for player to select 
-var playGame = function () { 
-    var playerChoice = window.prompt ("Enter Rock, Paper, or Scissor:");
-    
+//options for human to select 
+var playGame = function () {
+    var humanChoice = window.prompt("Enter Rock, Paper, or Scissor");
+
 }
+
+//random selection for computer
+var index = Math.floor(Math.random() * options.length);
+var computerChoice = options[index];
+
+window.alert("Computer chose: " + computerChoice);
+
+//make it a draws if human and computer choices are the same
+if (humanChoice === computerChoice) {
+    draws++
+    window.alert("It's a draw!");
+
+//check every win condition for human
+} else if (
+    (humanChoice === "Rock" && computerChoice === "Scissor") ||
+    (humanChoice === "Paper" && computerChoice === "Rock") ||
+    (humanChoice === "Scissor" && computerChoice === "Paper")
+) {
+    wins++;
+    window.alert ("You win!");
+
+//if above conditions failed, default human lose
+} else {
+    loses++;
+    window.alert("You lost!");
+}
+
+//score keeper for human vs computer
+window.alert(
+    "stats:\nWins: " + wins + "\nLosses: " + losses + "\nDraws: " + draws
+);
+
+//asking if human wants to play again
+var playAgain = window.confirm("Do you want to play again?");
+
+//if human press yes/ok, run the function again
+if (playAgain) {
+    playGame();
+};
+
+//run the game for the first time
+playGame();
+
