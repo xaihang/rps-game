05bb9bdf8f1bb2100702407485660e4683db9a08
xaihang <xaihang12@gmail.com>
2022-10-10 14:24:49 -0500

    added var and function

---
+++ script.js
@@ -0,0 +1,12 @@
+var win = 0;
+var lose = 0;
+var draw = 0;
+
+// options for computer to select 
+var options = ["Rock", "Paper", "Scissor"]
+
+//options for player to select 
+var playGame = function () { 
+    var playerChoice = window.prompt ("Enter Rock, Paper, or Scissor:");
+    
+}
